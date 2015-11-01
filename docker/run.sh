89a4759e26a4c0f9404f366373bef0e9b387c9a3
Doelia <kyxalia@gmail.com>
2015-11-01 11:27:35 +0100

    Branchement dump sql au docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 docker run \
     -p 3306:3306 \
-    -v ./deployed/:/docker-entrypoint-initdb.d \
+    -v $(pwd)/deployed:/docker-entrypoint-initdb.d \
     --name emp-mysql \
     -e MYSQL_ROOT_PASSWORD=gogoedt \
     -e MYSQL_DATABASE=edt \
