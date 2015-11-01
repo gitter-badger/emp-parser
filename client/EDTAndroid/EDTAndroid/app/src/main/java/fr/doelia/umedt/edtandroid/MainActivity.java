9c703e95a22f93ef1e3ce9a6a3012aa592a31135
Doelia <kyxalia@gmail.com>
2015-11-02 00:40:29 +0100

    Avancement base de test android

--- client/EDTAndroid/EDTAndroid/app/src/main/java/fr/doelia/umedt/edtandroid/MainActivity.java
+++ client/EDTAndroid/EDTAndroid/app/src/main/java/fr/doelia/umedt/edtandroid/MainActivity.java
@@ -9,44 +9,34 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Communicator communicator = new Communicator();
+        try {
+            communicator.getCreneaux(this.getTempListCreneaux());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private ArrayList<String> getTempListCreneaux() {
+        ArrayList<String> list = new  ArrayList<String>();
+        list.add("HLPH305");
+        list.add("HLLV301");
+        return list;
+    }
+
+
+
 }
