da01835ac690987994c70b7afe82408140f472e7
Doelia <kyxalia@gmail.com>
2015-11-01 11:06:41 +0100

    Ajout sql de déloiement

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,7 @@
-docker run -p 3306:3306 --name emp-mysql -e MYSQL_ROOT_PASSWORD=gogoedt -e MYSQL_DATABASE=edt -d mysql:latest
+docker run \
+    -p 3306:3306 \
+    -v ./deployed/:/docker-entrypoint-initdb.d \
+    --name emp-mysql \
+    -e MYSQL_ROOT_PASSWORD=gogoedt \
+    -e MYSQL_DATABASE=edt \
+    -d mysql:latest
