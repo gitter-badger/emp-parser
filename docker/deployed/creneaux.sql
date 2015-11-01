58b913e1fd101bc9463f6bfbeea2ad12d18539a5
Doelia <kyxalia@gmail.com>
2015-11-01 21:43:38 +0100

    SQL : Ajout d'un index sur la table des creneaux

--- docker/deployed/creneaux.sql
+++ docker/deployed/creneaux.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : Docker mysql
  Source Server Type    : MySQL
- Source Server Version : 50538
- Source Host           : localhost
+ Source Server Version : 50709
+ Source Host           : 192.168.99.103
  Source Database       : edt
 
  Target Server Type    : MySQL
- Target Server Version : 50538
+ Target Server Version : 50709
  File Encoding         : utf-8
 
- Date: 11/01/2015 11:08:08 AM
+ Date: 11/01/2015 21:43:01 PM
 */
 
 SET NAMES utf8;
@@ -24,7 +24,8 @@ DROP TABLE IF EXISTS `creneaux`;
 CREATE TABLE `creneaux` (
   `UE` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
+  `description` text,
+  KEY `ueindex` (`UE`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
