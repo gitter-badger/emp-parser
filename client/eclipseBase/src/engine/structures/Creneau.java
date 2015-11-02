db93538baee7b795c6131e9adb7194dbf33a3d04
Doelia <kyxalia@gmail.com>
2015-11-02 20:45:00 +0100

    Ajout méthode de recherche du prochain cours

--- client/eclipseBase/src/engine/structures/Creneau.java
+++ client/eclipseBase/src/engine/structures/Creneau.java
@@ -1,5 +1,7 @@
 package engine.structures;
 
+import java.util.ArrayList;
+
 /**
  * Created by doelia on 02/11/2015.
  */
@@ -8,9 +10,30 @@ public class Creneau {
     public String UE;
     public String location;
     public String description; // Correspond aussi la description de l'ue
+    public int timestamp;
     
     @Override
     public String toString() {
     	return UE + ", location: " + location;
+    }
+    
+    /**
+     * Retourne le creneau le plus récent qui n'est pas encore passé
+     * @throws CreneauNotFoundException 
+     */
+    public static Creneau getMostRecent(ArrayList<Creneau> list) throws CreneauNotFoundException {
+    	try {
+    		Creneau best = list.get(0);
+        	int todayTimestamp = 0;
+        	for (Creneau c : list) {
+        		if (c.timestamp < best.timestamp && c.timestamp < todayTimestamp) {
+        			best = c;
+        		}
+        	}
+        	return best;
+    	} catch (Exception e) {
+    		throw new CreneauNotFoundException();
+    	}
+    	
     }
 }
---
+++ client/eclipseBase/src/engine/structures/CreneauNotFoundException.java
@@ -0,0 +1,10 @@
+package engine.structures;
+
+public class CreneauNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
