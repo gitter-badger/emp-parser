4c31a79d287a6f40081a5dc367f155a5f8168e97
Doelia <kyxalia@gmail.com>
2016-01-03 16:32:22 +0100

    Netoyage des creneaux passés

--- client/MyApp/www/js/angular/MyCreneauxService.js
+++ client/MyApp/www/js/angular/MyCreneauxService.js
@@ -12,15 +12,27 @@ app.service('MyCreneaux', function($q,MyDatas, Fds, Storage, UserSettings) {
 
         this.GetCreneaux = function(callback) {
             if (UserSettings.GetSetting('realTimeData')) {
-                this.syncCreneaux(function(out) {
-                    callback(out);
+                this.syncCreneaux(function() {
+                    that.cleanCreneaux();
+                    callback(that.myCreneaux);
                 });
             } else {
                 this.syncCreneaux(function() {});
-                callback(this.myCreneaux);
+                that.cleanCreneaux();
+                callback(that.myCreneaux);
             }
         };
 
+        // Retire les creneaux passés en temps
+        this.cleanCreneaux = function() {
+            var todayTimestamp = Math.floor(new Date().getTime() / 1000);
+            this.myCreneaux = $.grep(this.myCreneaux, function(c) {
+                return c.DateEnd > todayTimestamp;
+            });
+            Storage.updateDb('mycreneaux', that.myCreneaux);
+        };
+
+        // Syncho avec le serveur, mise en localstorge et en RAM + appel du callback
         this.syncCreneaux = function(callback) {
             if (!this.syncInProgress) {
                 console.log("MyCreneaux:: syncCreneaux() ...");
@@ -31,7 +43,7 @@ app.service('MyCreneaux', function($q,MyDatas, Fds, Storage, UserSettings) {
                     Storage.updateDb('mycreneaux', that.myCreneaux);
                     that.syncInProgress = false;
                     console.log("MyCreneaux:: syncCreneaux() done");
-                    callback(out);
+                    callback();
                 });
             }
         };
@@ -48,8 +60,6 @@ app.service('MyCreneaux', function($q,MyDatas, Fds, Storage, UserSettings) {
                 deferred.resolve();
             });
         };
-
-
     };
 
     var o = new factory();
