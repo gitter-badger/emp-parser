1889e1ebeed2cffea6ceee5dbd13881e672b5947
Doelia <kyxalia@gmail.com>
2016-01-06 13:53:07 +0100

    Ajout script de mise en prod

---
+++ client/MyApp/compile.sh
@@ -0,0 +1,7 @@
+#keytool -genkey -v -keystore mika.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+path="platforms/android/build/outputs/apk/"
+
+cordova build android --release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore mika.keystore $path/android-release-unsigned.apk alias_name
+rm $path/android-release-unsigned.apk $path/go.apk
+/Users/doelia/Library/Android/sdk/build-tools/23.0.2/zipalign -v 4 $path/android-release-unsigned.apk $path/go.apk
